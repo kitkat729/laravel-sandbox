28bb3dbfa31609645ffb75c6552d4022ffd836c5
Hoi Yuen <hoikityuen@gmail.com>
2018-06-06 01:48:57 -0400

    dicerolling-#7 implemented DiceContainerInterface

--- lib/Models/MyDice.php
+++ lib/Models/MyDice.php
@@ -2,7 +2,29 @@
 
 namespace DeliveryDotCom\Models;
 
-class MyDice
-{
+use DeliveryDotCom\Contracts\DiceContainerInterface;
+use DeliveryDotCom\Contracts\DiceInterface;
 
+class MyDice implements DiceContainerInterface
+{
+  private $collection = [];
+
+  public function attach(DiceInterface $die)
+  {
+    $this->collection[] = $die;
+
+    return $this;
+  }
+
+  public function getTotal()
+  {
+    $total = 0;
+
+    foreach ($this->collection as $item)
+    {
+      $total += $item->roll();
+    }
+
+    return $total;
+  }
 }
