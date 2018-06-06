c1d719ee5c63a4c9f8f374fef7f67274f3836172
Hoi Yuen <hoikityuen@gmail.com>
2018-06-06 01:45:35 -0400

    dicerolling-#3 implemented dice rolling app

--- app/Http/Controllers/DiceRollingController.php
+++ app/Http/Controllers/DiceRollingController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DeliveryDotCom\Models\MyDice;
+use DeliveryDotCom\Models\DieFactory;
 
 class DiceRollingController extends Controller
 {
   public function index()
   {
+    $container = new MyDice();
+    $container->attach(DieFactory::create(10)); // 10-sided die
+    $container->attach(DieFactory::create(8)); // 8-sided die
+    $container->attach(DieFactory::create(6)); // 6-sided die
+    $container->attach(DieFactory::create(4)); // 4-sided die
+    $container->attach(DieFactory::create([0, 0, 1, 2, 3, 3])); // A die with arbitrary faces
+    $total = $container->getTotal();
 
+    echo "Total of all dice: $total\n";
   }
 }
