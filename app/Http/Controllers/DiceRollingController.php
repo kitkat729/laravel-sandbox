7f484835e495983af0a2d6ed629755490fde3b62
Hoi Yuen <hoikityuen@gmail.com>
2018-06-08 12:44:52 -0400

    dicerolling-#7 implement DI for MyDice container

--- app/Http/Controllers/DiceRollingController.php
+++ app/Http/Controllers/DiceRollingController.php
@@ -8,15 +8,21 @@ use DeliveryDotCom\Models\DieFactory;
 
 class DiceRollingController extends Controller
 {
+  protected $container;
+
+  public function __construct(MyDice $container = null)
+  {
+    $this->container = $container ?: new MyDice;
+  }
+
   public function index()
   {
-    $container = new MyDice();
-    $container->attach(DieFactory::create(10)); // 10-sided die
-    $container->attach(DieFactory::create(8)); // 8-sided die
-    $container->attach(DieFactory::create(6)); // 6-sided die
-    $container->attach(DieFactory::create(4)); // 4-sided die
-    $container->attach(DieFactory::create([0, 0, 1, 2, 3, 3])); // A die with arbitrary faces
-    $total = $container->getTotal();
+    $this->container->attach(DieFactory::create(10)); // 10-sided die
+    $this->container->attach(DieFactory::create(8)); // 8-sided die
+    $this->container->attach(DieFactory::create(6)); // 6-sided die
+    $this->container->attach(DieFactory::create(4)); // 4-sided die
+    $this->container->attach(DieFactory::create([0, 0, 1, 2, 3, 3])); // A die with arbitrary faces
+    $total = $this->container->getTotal();
 
     echo "Total of all dice: $total\n";
   }
