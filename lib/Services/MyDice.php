988291d896ca7778782fbd66be135597926830a6
Hoi Yuen <hoikityuen@gmail.com>
2018-06-12 00:02:08 -0400

    dicerolling-#7 added dice container service

---
+++ lib/Services/MyDice.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DeliveryDotCom\Services;
+
+use DeliveryDotCom\Contracts\DiceContainerInterface;
+use DeliveryDotCom\Contracts\DiceInterface;
+
+class MyDice implements DiceContainerInterface
+{
+  /**
+   * Dice in the container
+   *
+   * @var array
+   */
+  protected $items = [];
+
+  /**
+   * Attach a die to the container
+   *
+   * @param DiceInterface $die
+   * @return this
+   */
+  public function attach(DiceInterface $die)
+  {
+    $this->items[] = $die;
+
+    return $this;
+  }
+
+  /**
+   * Get the sum of all rolled dice
+   *
+   * @return int
+   */
+  public function getTotal()
+  {
+    $total = 0;
+
+    foreach ($this->items as $item)
+    {
+      $total += $item->roll();
+    }
+
+    return $total;
+  }
+}
